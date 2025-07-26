2bf6d7ad3befb951e7335c66861049c04b61df93
SATHVIK-CONNECT <sathvikpalivela0@gmail.com>
2025-07-26 23:48:49 +0530

    Files uploaded

---
+++ lib/screens/home.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+import 'package:todolist/constants/colors.dart';
+import 'package:todolist/widgets/todo_item.dart';
+import 'package:todolist/model/todo.dart';
+
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final todosList = ToDo.todoList();
+  List<ToDo> _foundToDo = [];
+  final _todoController = TextEditingController();
+
+  @override
+  void initState() {
+    _foundToDo = todosList;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Column(
+              children: [
+                searchBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 50, bottom: 20),
+                        // decoration: BoxDecoration(
+                        //   color: tdBlue,
+                        //   borderRadius: BorderRadius.circular(20),
+                        // ),
+                        child: Text(
+                          'My Tasks',
+                          style: (TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                          )),
+                        ),
+                      ),
+                      for (ToDo todo in _foundToDo.reversed)
+                        ToDoItem(
+                          todo: todo,
+                          onToDoChanged: _handleToDoChange,
+                          onDeleteItem: _deleteToDoItem,
+                        ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.only(left: 15),
+                    margin: EdgeInsets.only(bottom: 20, right: 20, left: 20),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0, 0.0),
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextField(
+
+                      controller: _todoController,
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.task_alt,color: tdBlue,),
+                        hintText: 'Add a new Task',
+
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 20, right: 20),
+                  child: ElevatedButton(
+
+                    onPressed: () {
+                      _addToDoItem(_todoController.text);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: tdBlue,
+                      minimumSize: Size(50, 50),
+                      elevation: 1,
+                    ),
+
+                    child: Text('+', style: TextStyle(fontSize: 40, color: Colors.white)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _handleToDoChange(ToDo todo) {
+    setState(() {
+      todo.isDone = !todo.isDone;
+    });
+  }
+
+  void _deleteToDoItem(String id) {
+    setState(() {
+      todosList.removeWhere((item) => item.id == id);
+    });
+  }
+
+  void _runFilter(String enteredKeyword) {
+    List<ToDo> results = [];
+    if (enteredKeyword.isEmpty) {
+      results = todosList;
+    } else {
+      results =
+          todosList
+              .where(
+                (item) => item.todoText!.toLowerCase().contains(
+                  enteredKeyword.toLowerCase(),
+                ),
+              )
+              .toList();
+    }
+
+    setState(() {
+      _foundToDo = results;
+    });
+  }
+
+  void _addToDoItem(String toDo) {
+    setState(() {
+      todosList.add(
+        ToDo(
+          id: DateTime.now().millisecondsSinceEpoch.toString(),
+          todoText: toDo,
+        ),
+      );
+    });
+    _todoController.clear();
+  }
+
+  Widget searchBox() {
+    return Container(
+      padding: EdgeInsets.only(left:20, top: 5),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: const [BoxShadow(
+          color: Colors.grey,
+          offset: Offset(0.0, 0.0),
+          blurRadius: 5.0,
+          spreadRadius: 0.0,
+        ),]
+      ),
+      child: TextField(
+        onChanged: (value) => _runFilter(value),
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(Icons.search, color: tdBlack, size: 20),
+          prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: tdGrey),
+
+        ),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(
+        // mainAxisAlignment: MainAxisAlignment.,
+        children: [
+          Icon(Icons.task, color: tdBlue, size: 30),
+          Container(
+            height: 30,
+            // padding: EdgeInsets.only(left:10, right: 10, top: 2),
+            margin: EdgeInsets.only(top:7, left: 10),
+            // width: ,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              // color: Colors.pink,
+            ),
+            child: const Text('Task Master', style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold, color: tdBlue
+            )),
+
+          ),
+        ],
+      ),
+    );
+  }
+}
